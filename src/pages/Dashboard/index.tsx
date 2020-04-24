49543d55d200de876ae123cb05574099e40ef798
Andreis3 <andrei.as3@hotmail.com>
2020-04-23 23:31:29 -0300

    create style dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório aqui" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38704247?s=460&u=4aae089250164c3650537a6ebc6e06e2508fab62&v=4"
+            alt="Andrei Santos"
+          />
+          <div>
+            <strong>andre/santos/unform</strong>
+            <p>gostack-challenge-typeorm-upload</p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38704247?s=460&u=4aae089250164c3650537a6ebc6e06e2508fab62&v=4"
+            alt="Andrei Santos"
+          />
+          <div>
+            <strong>andre/santos/unform</strong>
+            <p>gostack-challenge-typeorm-upload</p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38704247?s=460&u=4aae089250164c3650537a6ebc6e06e2508fab62&v=4"
+            alt="Andrei Santos"
+          />
+          <div>
+            <strong>andre/santos/unform</strong>
+            <p>gostack-challenge-typeorm-upload</p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
